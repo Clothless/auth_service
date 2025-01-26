09140dd85673861e02b824a275f108f8a897e7fe
Ibrahim Chaibedraa <m.chaibedraa@teletic.dz>
2025-01-26 20:55:00 +0100

    Middlewares

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const { secret } = require('../config/jwt');
+
+exports.authenticate = (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied' });
+    }
+    try {
+        const decoded = jwt.verify(token, secret);
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
+};
